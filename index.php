eebeb8250523f239543301907949f63c0d329fea
tanyaflint <tatiana.nedz@gmail.com>
2018-02-20 22:02:42 -0500

    Removed spaces on the home page

--- index.php
+++ index.php
@@ -6,8 +6,7 @@ require('logic.php');
 <html lang='en'>
 <head>
     <title>Bill Splitter - Project 2</title>
-
-    <meta charset='utf=8'>
+    <meta charset='utf-8'>
     <link rel='stylesheet'
           href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'
           integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u'
